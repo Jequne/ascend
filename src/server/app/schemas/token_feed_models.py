6df2afd9708bfae74d53a3d13b029cff02fe1860
Jequne <Privetpoka870@gmail.com>
2026-04-27 19:59:27 +0300

    updated token_feed_models.py schema

--- src/server/app/schemas/token_feed_models.py
+++ src/server/app/schemas/token_feed_models.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
 from datetime import datetime
-
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator
+from typing import Literal, Optional
 
 
 class DeployedToken(BaseModel):
@@ -28,30 +28,23 @@ class DeployedToken(BaseModel):
 
 
 class TokenFeedBase(BaseModel):
-    dev_holds_percent: float
-    snipers_hold_percent: float
+    blockchain: Literal["sol", "bsc"]
+    dev_holds_percent: Optional[float]
+    snipers_hold_percent: Optional[float]
     pair_address: str
     token_address: str
     token_image: str | None
     is_migrated: bool
     website: str | None
     telegram: str | None
-    discord: str | None
+    discord: str | None 
     twitter: str | None
     token_name: str
     token_ticker: str
-    twitter_admin_nickname: str | None
-    twitter_admin_id: str | None
+    twitter_admin_nickname: str | None = None
+    twitter_admin_id: str | None = None
     dev_wallet: str
     protocol: str
     last_deployed_tokens: list[DeployedToken] | None
     migrated_tokens_count: int
     all_tokens_count: int
-
-
-class TokenFeedSol(TokenFeedBase):
-    blockchain: str = "sol"
-
-
-class TokenFeedBsc(TokenFeedBase):
-    blockchain: str = "bsc"
